35ef67c3b20f604acfd2aac4337d70f0eee2a57f
xukaiqiang <xukaiqiang@ict.ac.cn>
2018-01-09 21:11:11 +0800

    新文件：   spiders/ifeng_spider.py 	新文件：   spiders/tencent_spider.py 	新文件：   spiders/toutiao_spider.py 	新文件：   stop_word.txt 	新文件：   update/scipts.py

---
+++ spiders/ifeng_spider.py
@@ -0,0 +1,128 @@
+# -*- coding:utf-8 -*-
+"""
+@author: xidongbo
+
+"""
+import bs4
+import datetime
+import simplejson as json
+
+import datasources
+import entities.news
+import entities.review
+import logs.loggers
+import spiders.base_spider
+import utils.utils
+
+logger = logs.loggers.LoggersHolder().get_logger("spiders")
+
+
+class IFengSpider(spiders.base_spider.BaseSpider):
+    def __init__(self):
+        super(IFengSpider, self).__init__()
+        self.fenghuang_news_roll_url = r'http://news.ifeng.com/listpage/7837/{}/{}/rtlist.shtml'
+        # docURL:sub_11111
+        self.fenghuang_review_roll_url = r'http://comment.ifeng.com/get.php?callback=newCommentListCallBack&orderby=&docUrl={}&format=js&job=1&p=1&pageSize=20&callback=newCommentListCallBack&skey=e0d9fe'
+        self.fenghuang_each_page_num = 60
+
+    def get_news(self, news_num):
+        session = datasources.get_db().create_session()
+        news_count = 0
+        for delta_day in range(news_num):
+            date = utils.utils.get_date_before(delta_day)
+            # 每天最多三页新闻：3*50
+            for page_index in range(1):
+                page_url = self.fenghuang_news_roll_url.format(date, page_index + 1, self.fenghuang_each_page_num)
+                try:
+                    # 设置headers,读取第i+1页的新闻数据
+                    page = self.get_response(referer='', url=page_url)
+                except Exception as e:
+                    logger.warning('Crawling Roll Failed: {}.'.format(page_url))
+                    continue
+                try:
+                    page_bs = bs4.BeautifulSoup(page, 'html.parser')
+                except Exception as e:
+                    logger.warning('Crawling Roll Failed: {}.'.format(page_url))
+                    continue
+                # ‘#’查找id名，‘.’查找class名 , 直接查找标签名
+                for news in page_bs.select('.newsList')[0].select('li'):
+                    '''
+                    #获取新闻信息：url,title,time
+                    '''
+                    news_obj = entities.news.NewsPlain()
+                    timestr = date[:4] + '-' + news.h4.text
+                    timestr = timestr.replace('/', '-')
+                    if timestr.index(' ') == len(timestr) - 5:
+                        timestr = timestr[:timestr.index(' ') + 1] + '0' + timestr[timestr.index(' ') + 1:]
+                    try:
+                        news_obj.time = datetime.datetime.strptime(timestr, "%Y-%m-%d %H:%M")
+                    except Exception as e:
+                        logger.warning('Crawling Roll Failed: {}.'.format(page_url))
+                        continue
+                    news_obj.url = news.a['href']
+                    if datasources.get_db().find_news_by_url(session, news_obj.url):
+                        continue
+                    news_obj.title = news.a.text
+                    news_obj.source = news_obj.SourceEnum.ifeng
+                    try:
+                        # 设置headers
+                        # 获取新闻正文页html
+                        news_html = self.get_response(referer='', url=news_obj.url)
+                        soup = bs4.BeautifulSoup(news_html, 'html.parser')
+                        # ‘#’查找id名，‘.’查找class名
+                        news_obj.media_name = soup.select('.ss03')[0].a.text
+                        news_obj.content = '\n'.join([p.text for p in soup.select('#artical_real')[0].select('p')])
+                        news_obj.abstract = news_obj.content[:100]
+                        review_info = news_html
+                        idx = review_info.index('commentUrl')
+                        review_info = review_info[idx + 13:]
+                        review_page_id = review_info[:review_info.index(',') - 1]
+                    except Exception as e:
+                        # set default
+                        logger.warning("Crawling Content Failed: {}".format(news_obj.url))
+                        continue
+                    logger.info("Crawling Content Success: {}".format(news_obj.url))
+                    # 若爬取评论失败，则为0
+                    news_obj.review_num = 0
+                    if review_page_id:
+                        try:
+                            review_url = self.fenghuang_review_roll_url.format(review_page_id)
+                            logger.info("Crawling Review Page Success: {}".format(review_url))
+                        except Exception as e:
+                            logger.warning("Crawling Review Page Failed: {}".format(review_url))
+                        try:
+                            review_page = self.get_response(
+                                referer='http://page.coral.qq.com/coralpage/comment/news.html',
+                                url=review_url)
+                            review_page = review_page[review_page.index('='):]
+                            left = review_page.index('{')
+                            right = review_page.rindex('}', 0, len(review_page) - 10) + 1
+                            review_page = review_page[left:right]
+                            jd = json.loads(review_page)
+                            news_obj.review_num = jd['count']
+                            valid_vevs = [rev for rev in jd['comments']]
+                            for rev in jd['comments']:
+                                valid_vevs += rev['parent']
+                            for review in valid_vevs:
+                                review_obj = entities.review.ReviewPlain()
+                                review_obj.user_id = review['user_id']
+                                review_obj.content = review['comment_contents']
+                                seconds = int(review['create_time'])
+                                review_obj.time = datetime.datetime.fromtimestamp(seconds)
+                                review_obj.agree = int(review['uptimes'])
+                                review_obj.user_name = review['uname']
+                                review_obj.area = review['ip_from']
+                                news_obj.reviews.append(review_obj)
+                        except Exception as e:  # 评论出错直接忽略
+                            logger.warning("An invalid comment.")
+                    utils.utils.remove_wild_char_in_news(news_obj)
+                    datasources.get_db().upsert_news_or_news_list(session, news_obj, commit_now=False)
+                    news_count += 1
+                    if news_count >= news_num:
+                        break
+                if news_count >= news_num:
+                    break
+            if news_count >= news_num:
+                break
+        datasources.get_db().commit_session(session)
+        datasources.get_db().close_session(session)
---
+++ spiders/toutiao_spider.py
@@ -0,0 +1,115 @@
+# -*- coding:utf-8 -*-
+"""
+@author: xidongbo
+
+"""
+import bs4
+import datetime
+import demjson
+import simplejson as json
+import random
+import time
+
+import datasources
+import entities.news
+import entities.review
+import logs.loggers
+import spiders.base_spider
+import utils.utils
+
+logger = logs.loggers.LoggersHolder().get_logger("spiders")
+
+
+class ToutiaoSpider(spiders.base_spider.BaseSpider):
+    def __init__(self):
+        super(ToutiaoSpider, self).__init__()
+        self.toutiao_news_roll_url = r'https://www.toutiao.com/api/pc/feed/?category=news_society&utm_source=toutiao&widen=1&max_behot_time={}&max_behot_time_tmp={}&tadrequire=true&as=A125BA93E6A7F96&cp=5A3697EF09466E1&_signature=kuBu4QAAyOGy.bX4veRHx5Lgbv'
+        # group_id={}&item_id={}
+        self.toutiao_review_roll_url = r'https://www.toutiao.com/api/comment/list/?group_id={}&item_id={}&offset=0&count=20'
+        self.toutiao_each_page_num = 7
+
+    def get_news(self, news_num):
+        session = datasources.get_db().create_session()
+        news_count = 0
+        now_time = int(time.time())
+        for i in range(news_num):
+            page_url = self.toutiao_news_roll_url.format(now_time, now_time)
+            try:
+                # 设置headers,读取第i+1页的新闻数据
+                page = self.get_response('https://www.toutiao.com/ch/news_society/', page_url)
+                page = page[page.index('{'):page.rindex('}') + 1]
+                # 转为json格式
+                jd = json.loads(page)
+                now_time = jd['next']['max_behot_time']
+            except Exception as e:
+                logger.warning('Crawling Roll Failed: {}.'.format(page_url))
+                now_time -= random.randint(0, 3600)
+                continue
+            logger.info('Crawling Roll Success: {}.'.format(page_url))
+            for news in jd['data']:
+                if news['is_feed_ad']:
+                    continue
+                news_obj = entities.news.NewsPlain()
+                try:
+                    news_obj.url = 'https://www.toutiao.com/a' + news['group_id']
+                    if datasources.get_db().find_news_by_url(session, news_obj.url):
+                        continue
+                    news_obj.title = news['title']
+                    news_obj.keywords = ','.join(news['label'])
+                    news_obj.media_name = news['source']
+                    if news_obj.media_name == '悟空问答':
+                        continue
+                    news_obj.abstract = news['abstract']
+                    news_obj.review_num = int(news['comments_count'])
+                    news_obj.source = news_obj.SourceEnum.toutiao
+
+                    # 设置headers
+                    # 获取新闻正文页html,提取news_content
+                    news_html = self.get_response(page_url, news_obj.url)
+                    news_html = news_html[news_html.index('articleInfo:'):news_html.rindex('commentInfo')]
+                    news_html = news_html[news_html.index('{'):news_html.rindex('}') + 1]
+                    dj = demjson.decode(news_html)
+                    soup = bs4.BeautifulSoup(dj['content'], 'html.parser').text
+                    soup = bs4.BeautifulSoup(soup, 'html.parser')
+                    # ‘#’查找id名，‘.’查找class名
+                    news_obj.time = datetime.datetime.strptime(dj['subInfo']['time'], "%Y-%m-%d %H:%M:%S")
+                    news_obj.content = '\n'.join([p.text for p in soup.select('p')])
+                    group_id = dj['groupId']
+                    item_id = dj['itemId']
+                    logger.info("Crawling Content Success: {}".format(news_obj.url))
+                except Exception as e:
+                    logger.warning("Crawling Content Failed: {}".format(news_obj.url))
+                    continue
+
+                review_url = self.toutiao_review_roll_url.format(group_id, item_id)
+                try:
+                    '''
+                    #获取评论信息：user_id,user_name,area,review_content,time,agree
+                    '''
+                    # self.review_num是真实的评论数量，可作为热度的参考值。
+                    # 但是评论内容最多取100条
+                    review_page = self.get_response('https://www.toutiao.com/a{}/'.format(group_id), review_url)
+                    jd = json.loads(review_page)
+                    news_obj.review_num = jd['data']['total']
+                    for review in jd['data']['comments']:
+                        review_obj = entities.review.ReviewPlain()
+                        review_obj.user_id = review['user']['user_id']
+                        review_obj.user_name = review['user']['name']
+                        review_obj.content = review['text']
+                        seconds = float(review['create_time'])
+                        review_obj.time = datetime.datetime.fromtimestamp(seconds)
+                        review_obj.agree = review['digg_count']
+                        news_obj.reviews.append(review_obj)
+                    logger.info("Crawling Review Page Success: {}".format(review_url))
+                except Exception as e:
+                    # 评论出错直接忽略
+                    logger.warning("Crawling Review Page Failed: {}".format(review_url))
+                utils.utils.remove_wild_char_in_news(news_obj)
+                datasources.get_db().upsert_news_or_news_list(session, news_obj, commit_now=False)
+                news_count += 1
+                if news_count >= news_num:
+                    break
+            if news_count >= news_num:
+                break
+        datasources.get_db().commit_session(session)
+        datasources.get_db().close_session(session)
